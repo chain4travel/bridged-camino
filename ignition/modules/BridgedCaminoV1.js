6fb67697aaf582ba7a272c7c5edb597a43c46e0a
Ekrem Seren <ekrem.seren@gmail.com>
2025-11-20 14:28:02 +0300

    Update deployment infrastructure (#6)
    * update networks list
    
    * transfer ownership after set master minter
    
    * add deploy hardhat task
    
    * move deploy under cam scope
    
    * rename the task script
    
    * switch to using deployment id
    
    * lint
    
    * naming for official cam token
    
    * lint

--- ignition/modules/BridgedCaminoV1.js
+++ ignition/modules/BridgedCaminoV1.js
@@ -95,11 +95,17 @@ module.exports = buildModule("BridgedCaminoV1Module", (m) => {
     // Step 4: Set the minter manager on MasterMinter now that the token is deployed
     // This connects the MasterMinter to the token
     // Deployer can call this because they are the temporary owner
-    m.call(masterMinter, "setMinterManager", [bridgedCaminoV1Proxy], { id: "SetMinterManagerOnMasterMinter" });
+    const setMinterManagerCall = m.call(masterMinter, "setMinterManager", [bridgedCaminoV1Proxy], {
+        id: "SetMinterManagerOnMasterMinter",
+    });
 
     // Step 5: Transfer ownership of MasterMinter to the final owner (multisig)
     // After this, deployer has no control over MasterMinter
-    m.call(masterMinter, "transferOwnership", [masterMinterOwner], { id: "TransferMasterMinterOwnership" });
+    // IMPORTANT: This must happen AFTER setMinterManager, so we add it as a dependency
+    m.call(masterMinter, "transferOwnership", [masterMinterOwner], {
+        id: "TransferMasterMinterOwnership",
+        after: [setMinterManagerCall],
+    });
 
     return { bridgedCaminoV1Proxy, bridgedCaminoV1Impl, masterMinter };
 });
